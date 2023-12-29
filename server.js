9c5a13e975ed9dfb481fc1e589368c5fb0ff43bc
medb2m <79202818+medb2m@users.noreply.github.com>
2023-12-29 06:49:19 +0100

    first commit, server creation & database connection

---
+++ server.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+//Database Connection
+/* mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', (error)=> console.log(error))
+db.on('open', ()=> console.log('Connected to Database')) */
+
+mongoose.connect('mongodb://127.0.0.1:27017/subs').then(()=> {
+    console.log('Connected to Database !')
+}).catch(err =>{
+    console.log(err)
+}) 
+
+app.use(express.json())
+
+const subsRouter = require('./routes/subs')
+
+app.listen(3000, ()=>{
+    console.log('Server Started !')
+})
