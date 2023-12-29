f20a8d6da42b4c21a735f7d1ef252afd61e4650d
medb2m <79202818+medb2m@users.noreply.github.com>
2023-12-30 00:21:37 +0100

    Created getOne, updateOne, DeleteOne()

--- routes/subs.js
+++ routes/subs.js
@@ -12,8 +12,8 @@ router.get('/', async (req,res) => {
     }
 })
 // Getting One
-router.get('/:id', (req,res) => {
-    res.send(req.params.id) 
+router.get('/:id', getSub, (req,res) => {
+    res.json(res.sub) 
 })
 // Creating One 
 router.post('/', async (req,res) => {
@@ -29,12 +29,42 @@ router.post('/', async (req,res) => {
     }
 })
 // Updating One
-router.patch('/:id', (req,res) => {
-    
+router.patch('/:id', getSub, async (req,res) => {
+    if (req.body.name != null){
+        res.sub.name = req.body.name
+    }
+    if (req.body.subscribedToChanel != null){
+        res.sub.subscribedToChanel = req.body.subscribedToChanel
+    }
+    try {
+        const updatedSub = await res.sub.save()
+        res.json(updatedSub)
+    } catch (err) {
+        res.status(400).json({ message : err.message})
+    }
 })
 // Deleting One 
-router.delete('/:id', (req,res) => {
-    
+router.delete('/:id', getSub, async (req,res) => {
+    try {
+        await res.sub.deleteOne()
+        res.json({message : `${res.sub.name} Deleted !`})
+    } catch (err) {
+        res.status(500).json({message : err.message})
+    }
 })
+
+// Middleware to get subscriber by ID
+async function getSub (req, res, next) {
+    let sub
+    try {
+        sub = await Sub.findById(req.params.id)
+        if (sub == null) {
+            return res.status(404).json({ message : 'Cannot find subscriber'})}
+    } catch (err) {
+        return res.status(500).json({message : err.message})
+    }
+    res.sub = sub
+    next()
+}
 
 module.exports = router
